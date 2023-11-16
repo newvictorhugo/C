8e3a918079868f2fefc255ab5976f8eb2530b6e8
Victor Hugo <victorhugo10.2003@hotmail.com>
2023-11-16 16:28:16 -0300

    Summary

--- 2° Semestre/4° Bimestre/Projeto adm ong/projADMONG.cpp
+++ 2° Semestre/4° Bimestre/Projeto adm ong/projADMONG.cpp
@@ -5,7 +5,7 @@
 #include<stdbool.h>
 
 #define MAX_STR 100
-#define MAX_STR_X5 500
+#define MAX_STR_X10 1000
 #define TF_VOL 10
 #define TF_ANI 10
 #define TF_EMP 10
@@ -26,12 +26,12 @@ typedef struct{
 
 typedef struct{
 	int vCod[TF_EMP], TLE = 0, cod_emp;
-	char vEmpresa[MAX_STR], marcacao[MAX_STR_X5], descricao[MAX_STR_X5];
+	char vEmpresa[MAX_STR], marcacao[MAX_STR_X10], descricao[MAX_STR_X10];
 }empresa;
 
 typedef struct{
 	int vProj[TF_PROJ], TLP = 0, cod_proj;
-	char vProjeto[MAX_STR], descricao[MAX_STR_X5], marcacao[MAX_STR_X5];
+	char vProjeto[MAX_STR], descricao[MAX_STR_X10], marcacao[MAX_STR_X10];
 }projeto;
 
 typedef struct{
@@ -200,22 +200,22 @@ int main(){
 							
 						case 2:
 							system("cls");
-							
+							exclusaoAnimal(&a);
 							break;
 							
 						case 3:
 							system("cls");
-							
+							exclusaoEmpresa(&a);
 							break;
 							
 						case 4:
 							system("cls");
-							
+							exclusaoProjeto(&a);
 							break;
 							
 						case 5:
 							system("cls");
-							
+							exclusaoDoacao(&a);
 							break;
 							
 						case 0:
@@ -237,27 +237,27 @@ int main(){
 						
 						case 1:
 							system("cls");
-							
+							alterarVoluntario(&a);
 							break;
 							
 						case 2:
 							system("cls");
-							
+							alterarAnimal(&a);
 							break;
 							
 						case 3:
 							system("cls");
-							
+							alterarEmpresa(&a);
 							break;
 							
 						case 4:
 							system("cls");
-							
+							alterarProjeto(&a);
 							break;
 							
 						case 5:
 							system("cls");
-							
+							alterarDoacao(&a);
 							break;
 							
 						case 0:
@@ -437,7 +437,7 @@ void cadastrarAnimal(menuInfoStruct *a){
 		scanf("%c", &a->ani.castrado);
 		getchar();
 				
-		fprintf(pont, "\nCod: %d\nNome: %s\nNascimento: %d/%d/%d\nSexo: %c\nEspecie: %s\nRaca: %s\nCastrado %c\n", a->ani.vNumAni[indice], a->ani.vAnimal, a->data.dia, a->data.mes, a->data.ano, a->ani.sexo, a->ani.especie, a->ani.raca, a->ani.castrado);
+		fprintf(pont, "\nCod: %d\nNome: %s\nNascimento: %d/%d/%d\nSexo: %c\nEspecie: %s\nRaca: %s\nCastrado: %c\n", a->ani.vNumAni[indice], a->ani.vAnimal, a->data.dia, a->data.mes, a->data.ano, a->ani.sexo, a->ani.especie, a->ani.raca, a->ani.castrado);
 		
 		do{
 			printf("Continuar?(s/n): ");
@@ -623,10 +623,9 @@ void cadastrarDoacao(menuInfoStruct *a){
 
 //----------EXCLUSAO----------//
 
-
 void exclusaoVoluntario(menuInfoStruct *a){
 	FILE *pont, *temp;
-	char buffer[255];
+	char buffer[MAX_STR_X10];
 	int cod, codBusca;
 	int encontrado = 0;
 
@@ -646,12 +645,12 @@ void exclusaoVoluntario(menuInfoStruct *a){
 	printf("Digite o codigo do voluntario: ");
 	scanf("%d", &codBusca);
 
-	while(fgets(buffer, 255, pont) != NULL){
+	while(fgets(buffer, MAX_STR_X10, pont) != NULL){
 		if(sscanf(buffer, "Cod: %d", &cod) == 1){
 			if(cod == codBusca){
 				encontrado = 1;
-				printf("\nEncontrado:\n%s", buffer); // Mostrando as informações do voluntário
-				while(fgets(buffer, 255, pont) && strncmp(buffer, "\n", 1) != 0){
+				printf("\nEncontrado:\n%s", buffer);
+				while(fgets(buffer, MAX_STR_X10, pont) && strncmp(buffer, "\n", 1) != 0){
 					printf("%s", buffer);
 				}
 				do{
@@ -660,7 +659,7 @@ void exclusaoVoluntario(menuInfoStruct *a){
 					scanf(" %c", &confirma);
 				
 					if(confirma == 's' || confirma == 'S'){
-						continue; // Não escreve no arquivo temporário
+						continue;
 					}else if(confirma == 'n' || confirma == 'N'){
 						encontrado = 2;
 					}else{
@@ -676,7 +675,7 @@ void exclusaoVoluntario(menuInfoStruct *a){
 
 	fclose(pont);
 	fclose(temp);
-
+	system("cls");
 	if(encontrado == 1){
 		remove("cadvoluntario.txt");
 		rename("temp.txt", "cadvoluntario.txt");
@@ -687,30 +686,379 @@ void exclusaoVoluntario(menuInfoStruct *a){
 	}else{
 		remove("temp.txt");
 	}
-	system("cls");
 }
 
 void exclusaoAnimal(menuInfoStruct *a){
-	
-	
+	FILE *pont, *temp;
+	char buffer[MAX_STR_X10];
+	int cod, codBusca;
+	int encontrado = 0;
+
+	pont = fopen("cadanimal.txt", "r");
+	if(pont == NULL){
+		printf("\nErro ao acessar o arquivo!!\n");
+		return;
+	}
+
+	temp = fopen("temp.txt", "w");
+	if(temp == NULL){
+		printf("\nErro ao criar arquivo temporario!!\n");
+		fclose(pont);
+		return;
+	}
+
+	printf("Digite o codigo do animal: ");
+	scanf("%d", &codBusca);
+
+	while(fgets(buffer, MAX_STR_X10, pont) != NULL){
+		if(sscanf(buffer, "Cod: %d", &cod) == 1){
+			if(cod == codBusca){
+				encontrado = 1;
+				printf("\nEncontrado:\n%s", buffer);
+				while(fgets(buffer, MAX_STR_X10, pont) && strncmp(buffer, "\n", 1) != 0){
+					printf("%s", buffer);
+				}
+				do{
+					printf("\nDeseja realmente excluir? (s/n): ");
+					char confirma;
+					scanf(" %c", &confirma);
+				
+					if(confirma == 's' || confirma == 'S'){
+						continue;
+					}else if(confirma == 'n' || confirma == 'N'){
+						encontrado = 2;
+					}else{
+						encontrado = 3;
+						printf("\nOpcao invalida\n");
+					}
+				}while(encontrado==3);
+				
+			}
+		}
+		fprintf(temp, "%s", buffer);
+	}
+
+	fclose(pont);
+	fclose(temp);
+	system("cls");
+	if(encontrado == 1){
+		remove("cadanimal.txt");
+		rename("temp.txt", "cadanimal.txt");
+		printf("Animal excluido com sucesso!\n");
+	} else if(encontrado == 0){
+		remove("temp.txt");
+		printf("Codigo nao encontrado.\n");
+	}else{
+		remove("temp.txt");
+	}
 }
 void exclusaoEmpresa(menuInfoStruct *a){
-	
-	
+	FILE *pont, *temp;
+	char buffer[MAX_STR_X10];
+	int cod, codBusca, validar = 0;
+	bool encontrado = false, confirmado = false;
+
+	pont = fopen("cadempresa.txt", "r");
+	if(pont == NULL){
+		printf("\nErro ao acessar o arquivo!!\n");
+		return;
+	}
+
+	temp = fopen("temp.txt", "w");
+	if(temp == NULL){
+		printf("\nErro ao criar arquivo temporario!!\n");
+		fclose(pont);
+		return;
+	}
+
+	printf("Digite o codigo da empresa: ");
+	scanf("%d", &codBusca);
+	getchar();
+
+	while(fgets(buffer, MAX_STR_X10, pont) != NULL){
+		if(encontrado && strncmp(buffer, "\n", 1) == 0) {
+			confirmado = false;
+		}
+
+		if(sscanf(buffer, "Cod: %d", &cod) == 1 && cod == codBusca){
+			encontrado = true;
+			printf("\nEncontrado:\n%s", buffer);
+			while(fgets(buffer, MAX_STR_X10, pont) && strncmp(buffer, "\n", 1) != 0){
+				printf("%s", buffer);
+			}
+			char confirma;
+			do {
+				printf("\nDeseja realmente excluir? (s/n): ");
+				scanf(" %c", &confirma);
+				getchar();
+
+				if(confirma == 's' || confirma == 'S'){
+					confirmado = true;
+					validar = 1;
+					break;
+				} else if(confirma == 'n' || confirma == 'N'){
+					validar = 2;
+					break;
+				} else {
+					printf("\nOpcao invalida\n");
+				}
+			} while(true);
+		}
+
+		if(!encontrado || !confirmado) {
+			fprintf(temp, "%s", buffer);
+		}
+	}
+
+	fclose(pont);
+	fclose(temp);
+	system("cls");
+	if(validar == 1){
+		remove("cadempresa.txt");
+		rename("temp.txt", "cadempresa.txt");
+		printf("Empresa excluida com sucesso!\n");
+	} else if(validar == 2){
+		remove("temp.txt");
+	}else{
+		remove("temp.txt");
+		printf("Codigo nao encontrado.\n");
+	}
 }
+
 void exclusaoProjeto(menuInfoStruct *a){
-	
-	
+	FILE *pont, *temp;
+	char buffer[MAX_STR_X10];
+	int cod, codBusca, validar = 0;
+	bool encontrado = false, confirmado = false;
+
+	pont = fopen("cadprojeto.txt", "r");
+	if(pont == NULL){
+		printf("\nErro ao acessar o arquivo!!\n");
+		return;
+	}
+
+	temp = fopen("temp.txt", "w");
+	if(temp == NULL){
+		printf("\nErro ao criar arquivo temporario!!\n");
+		fclose(pont);
+		return;
+	}
+
+	printf("Digite o codigo do projeto: ");
+	scanf("%d", &codBusca);
+	getchar();
+
+	while(fgets(buffer, MAX_STR_X10, pont) != NULL){
+		if(encontrado && strncmp(buffer, "\n", 1) == 0) {
+			confirmado = false;
+		}
+
+		if(sscanf(buffer, "Cod: %d", &cod) == 1 && cod == codBusca){
+			encontrado = true;
+			printf("\nEncontrado:\n%s", buffer);
+			while(fgets(buffer, MAX_STR_X10, pont) && strncmp(buffer, "\n", 1) != 0){
+				printf("%s", buffer);
+			}
+			char confirma;
+			do {
+				printf("\nDeseja realmente excluir? (s/n): ");
+				scanf(" %c", &confirma);
+				getchar();
+
+				if(confirma == 's' || confirma == 'S'){
+					confirmado = true;
+					validar = 1;
+					break;
+				} else if(confirma == 'n' || confirma == 'N'){
+					validar = 2;
+					break;
+				} else {
+					printf("\nOpcao invalida\n");
+				}
+			} while(true);
+		}
+
+		if(!encontrado || !confirmado) {
+			fprintf(temp, "%s", buffer);
+		}
+	}
+
+	fclose(pont);
+	fclose(temp);
+	system("cls");
+	if(validar == 1){
+		remove("cadprojeto.txt");
+		rename("temp.txt", "cadprojeto.txt");
+		printf("Projeto excluida com sucesso!\n");
+	} else if(validar == 2){
+		remove("temp.txt");
+	}else{
+		remove("temp.txt");
+		printf("Codigo nao encontrado.\n");
+	}
 }
 void exclusaoDoacao(menuInfoStruct*a){
-	
-	
+	FILE *pont, *temp;
+	char buffer[MAX_STR_X10];
+	int cod, codBusca, validar = 0;
+	bool encontrado = false, confirmado = false;
+
+	pont = fopen("caddoacao.txt", "r");
+	if(pont == NULL){
+		printf("\nErro ao acessar o arquivo!!\n");
+		return;
+	}
+
+	temp = fopen("temp.txt", "w");
+	if(temp == NULL){
+		printf("\nErro ao criar arquivo temporario!!\n");
+		fclose(pont);
+		return;
+	}
+
+	printf("Digite o codigo da doacao: ");
+	scanf("%d", &codBusca);
+	getchar();
+
+	while(fgets(buffer, MAX_STR_X10, pont) != NULL){
+		if(encontrado && strncmp(buffer, "\n", 1) == 0) {
+			confirmado = false;
+		}
+
+		if(sscanf(buffer, "Cod: %d", &cod) == 1 && cod == codBusca){
+			encontrado = true;
+			printf("\nEncontrado:\n%s", buffer);
+			while(fgets(buffer, MAX_STR_X10, pont) && strncmp(buffer, "\n", 1) != 0){
+				printf("%s", buffer);
+			}
+			char confirma;
+			do {
+				printf("\nDeseja realmente excluir? (s/n): ");
+				scanf(" %c", &confirma);
+				getchar();
+
+				if(confirma == 's' || confirma == 'S'){
+					confirmado = true;
+					validar = 1;
+					break;
+				} else if(confirma == 'n' || confirma == 'N'){
+					validar = 2;
+					break;
+				} else {
+					printf("\nOpcao invalida\n");
+				}
+			} while(true);
+		}
+
+		if(!encontrado && !confirmado) {
+			fprintf(temp, "%s", buffer);
+		}
+	}
+
+	fclose(pont);
+	fclose(temp);
+	system("cls");
+	if(validar == 1){
+		remove("caddoacao.txt");
+		rename("temp.txt", "caddoacao.txt");
+		printf("Doacao excluida com sucesso!\n");
+	} else if(validar == 2){
+		remove("temp.txt");
+	}else{
+		remove("temp.txt");
+		printf("Codigo nao encontrado.\n");
+	}
 }
 
 //----------FIM DE EXCLUSAO----------//
 
 
 
+//----------ALTERAR----------//
+
+void alterarVoluntario(menuInfoStruct *a){
+    FILE *pont, *temp;
+    char buffer[MAX_STR_X10];
+    int cod, codBusca;
+    bool encontrado = false;
+
+    pont = fopen("cadvoluntario.txt", "r");
+    if(pont == NULL){
+        printf("\nErro ao acessar o arquivo!!\n");
+        return;
+    }
+
+    temp = fopen("temp.txt", "w");
+    if(temp == NULL){
+        printf("\nErro ao criar arquivo temporario!!\n");
+        fclose(pont);
+        return;
+    }
+
+    printf("Digite o codigo do voluntario: ");
+    scanf("%d", &codBusca);
+    getchar(); // Para consumir o '\n' remanescente no buffer de entrada
+
+    while(fgets(buffer, MAX_STR_X10, pont) != NULL){
+        if(sscanf(buffer, "Cod: %d", &cod) == 1){
+            if(cod == codBusca){
+                encontrado = true;
+                printf("\nEncontrado:\n%s", buffer);
+                fprintf(temp, "%s", buffer); // Escreve o código no arquivo temporário
+
+                // Exibir e capturar novas informações
+                printf("Nome do Voluntario (mantido): ");
+                fgets(buffer, MAX_STR_X10, pont); // Ler o nome do arquivo original
+                printf("%s", buffer); // Exibir o nome
+                fprintf(temp, "%s", buffer); // Escrever o nome no arquivo temporário
+
+                printf("Digite o novo numero de celular: ");
+                fgets(a->vol.cel, MAX_STR, stdin);
+                a->vol.cel[strcspn(a->vol.cel, "\n")] = 0;
+                fprintf(temp, "Celular: %s\n", a->vol.cel);
+
+                printf("Digite o novo email: ");
+                fgets(a->vol.email, MAX_STR, stdin);
+                a->vol.email[strcspn(a->vol.email, "\n")] = 0;
+                fprintf(temp, "Email: %s\n", a->vol.email);
+
+                // Pular as linhas antigas de celular e email no arquivo original
+                fgets(buffer, MAX_STR_X10, pont);
+                fgets(buffer, MAX_STR_X10, pont);
+
+                continue; // Pular para a próxima iteração para evitar escrever as informações antigas
+            }
+        }
+        fprintf(temp, "%s", buffer);
+    }
+
+    fclose(pont);
+    fclose(temp);
+
+    if(encontrado){
+        remove("cadvoluntario.txt");
+        rename("temp.txt", "cadvoluntario.txt");
+        printf("Informacoes do voluntario atualizadas com sucesso!\n");
+    } else {
+        remove("temp.txt");
+        printf("Codigo nao encontrado.\n");
+    }
+}
+
+void alterarAnimal(menuInfoStruct *a){
+	
+}
+void alterarEmpresa(menuInfoStruct *a){
+	
+}
+void alterarProjeto(menuInfoStruct *a){
+	
+}
+void alterarDoacao(menuInfoStruct*a){
+	
+}
+
+//----------FIM DE ALTERAR----------//
 
 
 
